e9527a755959a3e9a702f5e216f54d298c62db15
vvyre <seung.yoon.yu@gmail.com>
2025-09-20 14:51:18 +0900

    프로젝트 초기화 및 기본 설정

---
+++ app/manifest.ts
@@ -0,0 +1,19 @@
+import meta from 'assets/meta'
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: meta.title,
+    short_name: meta.title,
+    description: meta.description,
+    start_url: '/',
+    display: 'standalone',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+    ],
+  }
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {}
 
-export default nextConfig;
+const vExPlugin = createVanillaExtractPlugin()
+const config = compose(vExPlugin)
+
+export default config
+
+/**
+ * 여러 개의 플러그인을 배열의 끝부터 순차 적용시킵니다.
+ * [a,b,c]일 때 a(b(c(nextConfig)))의 형태가 됩니다.
+ */
+type PluginFn = (config: NextConfig) => NextConfig
+function compose(...fns: PluginFn[]) {
+  return fns.reduceRight((acc, fn) => fn(acc), nextConfig)
+}
