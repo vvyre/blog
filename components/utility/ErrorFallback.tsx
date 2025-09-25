59715948784f8591d5a46bc11cbfa6abd3e9ed2e
vvyre <seung.yoon.yu@gmail.com>
2025-09-25 16:58:48 +0900

    유틸리티 컴포넌트 디렉토리 이름을 utils와 구분되도록 변경

---
+++ components/utility/ErrorFallback.tsx
@@ -0,0 +1,13 @@
+export function ErrorFallback({ message, error = null }: { message?: string; error?: Error | null }) {
+  return (
+    <></>
+    // <View className={BASE}>
+    //   <Heading as="h1">{error?.message ?? message ?? '알 수 없는 오류입니다'}</Heading>
+    //   <Spacing size="1rem" />
+
+    //   <Btn as="Link" href="/" className={ERROR_BTN}>
+    //     돌아가기
+    //   </Btn>
+    // </View>
+  )
+}
---
+++ components/utility/Wrapper.tsx
@@ -0,0 +1,6 @@
+import { cloneElement, JSX, type ReactNode } from 'react'
+
+export function Wrapper({ as, condition = true, children }: { as: () => ReactNode & JSX.Element; condition?: boolean; children?: ReactNode }) {
+  if (!condition) return children
+  else return cloneElement(as(), {}, children)
+}
