316bda1dd3d3b45b5d133b19526ceabefefef048
vvyre <seung.yoon.yu@gmail.com>
2025-10-17 14:54:39 +0900

    미사용 코드 삭제, 기존 코드 리팩토링

--- features/navigation/containers/Navigation.tsx
+++ features/navigation/containers/Navigation.tsx
@@ -1,12 +1,7 @@
 'use client'
-import { HamburgerMenuIcon, Link2Icon } from '@radix-ui/react-icons'
-import { useMediaQuery } from 'hooks/useMediaQuery.hook'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import { ComponentProps, useContext } from 'react'
+import { useContext } from 'react'
 import { ExpandedNav } from '../components/ExpandedNav'
-import { Menu } from './Menu'
+import { HamburgerMenu, HamburgerMenuBtn } from './HamburgerMenu'
 import * as css from './Navigation.css'
 import { NavContext, NavProvider } from './NavigationProvider'
 
@@ -19,66 +14,21 @@ export function Navigation() {
 }
 
 function NavigationContent() {
-  const isMobile = useMediaQuery('(max-width: 600px)')
-  const { isOpen, setOpenState } = useContext(NavContext)
+  const { key, setOpenState } = useContext(NavContext)
+  const handleMenuClose = () => setOpenState({ type: 'close' })
+  const handleHamburgerButton = () => (key === 'hamburger' ? handleMenuClose() : setOpenState({ type: 'open', key: 'hamburger' }))
 
   return (
     <>
       <div className={css.frame}>
-        {!isMobile ? (
-          <div>
-            <HamburgerMenuBtn
-              onClick={e => {
-                e.stopPropagation()
-                setOpenState({ type: 'toggle' })
-              }}
-            />
-          </div>
-        ) : (
-          <HamburgerMenuBtn
-            onClick={e => {
-              e.stopPropagation()
-              setOpenState({ type: 'toggle' })
-            }}
-          />
-        )}
+        <HamburgerMenuBtn
+          onClick={e => {
+            e.stopPropagation()
+            handleHamburgerButton()
+          }}
+        />
       </div>
-      <ExpandedNav
-        isOpen={isOpen}
-        onClose={() => setOpenState({ type: 'close' })}
-        content={<Menu onClose={() => setOpenState({ type: 'close' })} />}
-      />
+      <ExpandedNav isOpen={!key === null} onClose={() => handleMenuClose()} content={<HamburgerMenu onClose={() => handleMenuClose()} />} />
     </>
-  )
-}
-
-function LeftMenuGroup() {
-  const isMobile = useMediaQuery('(max-width: 600px)')
-  const ICON_COLOR = isMobile ? 'grey' : 'black'
-  const path = usePathname()
-  const isStartPage = path === '/'
-
-  return (
-    <>
-      <button type="button" className={css.categoryBtn}>
-        <Link href="/">
-          <Image unoptimized src="" alt="logo" />
-        </Link>
-      </button>
-
-      {!isStartPage && (
-        <button type="button" className={css.categoryBtn}>
-          <Link2Icon color={ICON_COLOR} />
-        </button>
-      )}
-    </>
-  )
-}
-
-function HamburgerMenuBtn({ ...props }: ComponentProps<'button'>) {
-  return (
-    <button type="button" data-menu-trigger onClick={props.onClick} className={css.categoryBtn}>
-      <HamburgerMenuIcon color="white" width="21" height="21" />
-    </button>
   )
 }
