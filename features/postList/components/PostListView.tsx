2f5b3f59b0d1afca6a709990492b95b294785004
vvyre <seung.yoon.yu@gmail.com>
2025-10-20 19:18:53 +0900

    기존 로직 삭제 및 스타일 수정

--- features/postList/components/PostListView.tsx
+++ features/postList/components/PostListView.tsx
@@ -1,26 +1,17 @@
 import type { NotionPageMeta } from 'features/notion'
+import { pageMeta } from 'features/notion/utils/pageMeta.util'
 import Link from 'next/link'
 import * as css from './PostListView.css'
-import { pageMeta } from 'features/notion/utils/pageMeta.util'
 
-export function PostListView({ posts, years }: { posts: Record<number, NotionPageMeta[]>; years: number[] }) {
+export function PostListView({ posts }: { posts: NotionPageMeta[] }) {
   return (
-    <>
-      <div className={css.postListFrame}>
-        {years.map(y => (
-          <div key={y} className={css.viewGroupFrame}>
-            <div className={css.viewYear}>
-              <div className={css.yearText}>{y}</div>
-            </div>
-            <div className={css.viewLink}>
-              {posts[y].map(p => (
-                <PostLink key={p.id} meta={p} />
-              ))}
-            </div>
-          </div>
+    <div className={css.postListFrame}>
+      <div className={css.viewLink}>
+        {posts.map(p => (
+          <PostLink key={p.id} meta={p} />
         ))}
       </div>
-    </>
+    </div>
   )
 }
 
