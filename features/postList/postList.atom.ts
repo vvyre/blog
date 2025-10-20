e3d1626bf472cff962e3408e1df148148309d307
vvyre <seung.yoon.yu@gmail.com>
2025-10-20 19:17:26 +0900

    랜덤 5개의 포스트를 찾는 로직 작성

--- features/postList/postList.atom.ts
+++ features/postList/postList.atom.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs'
 import type { NotionPageMeta } from 'features/notion'
 import { atom } from 'jotai'
+import { pick } from 'utils/pick'
 
 export const postsAtom = atom<NotionPageMeta[]>([])
 
@@ -16,3 +17,12 @@ export const postGroupByYearAtom = atom<Record<string, NotionPageMeta[]>>(get =>
   )
 )
 export const yearsAtom = atom<number[]>(get => Object.keys(get(postGroupByYearAtom)).map(Number).reverse())
+
+export const shuffleAtom = atom(0)
+export const randomPostsAtom = atom<NotionPageMeta[]>(get => {
+  const posts = get(postsAtom)
+  if (!posts.length) return []
+  get(shuffleAtom)
+  const selected = pick(5, [0, posts.length - 1])
+  return selected.map(num => posts[num]).sort((p1, p2) => dayjs(p2.properties.date.date?.start).year() - dayjs(p1.properties.date.date?.start).year())
+})
