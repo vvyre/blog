0e14e44d525bfba5515e7f8798fdf2ce125d150a
vvyre <seung.yoon.yu@gmail.com>
2025-10-20 18:09:34 +0900

    햄버거 메뉴 리팩토링 및 스타일 적용

--- features/navigation/containers/HamburgerMenu.tsx
+++ features/navigation/containers/HamburgerMenu.tsx
@@ -1,19 +1,10 @@
 'use client'
-import { HamburgerMenuIcon } from '@radix-ui/react-icons'
 import { pageMeta } from 'features/notion/utils/pageMeta.util'
 import { postGroupByYearAtom, yearsAtom } from 'features/postList/postList.atom'
 import { useAtomValue } from 'jotai'
 import Link from 'next/link'
 import type { ComponentProps } from 'react'
-import * as css from './Navigation.css'
-
-export function HamburgerMenuBtn({ ...props }: ComponentProps<'button'>) {
-  return (
-    <button type="button" data-menu-trigger onClick={props.onClick} className={css.categoryBtn}>
-      <HamburgerMenuIcon color="white" width="21" height="21" />
-    </button>
-  )
-}
+import * as css from './HamburgerMenu.css'
 
 export function HamburgerMenu({ onClose }: { onClose: () => void } & ComponentProps<'ul'>) {
   const YEAR_GROUPED_POSTS = useAtomValue(postGroupByYearAtom)
@@ -21,15 +12,20 @@ export function HamburgerMenu({ onClose }: { onClose: () => void } & ComponentPr
 
   return (
     <ul className={css.menuList}>
-      {YEARS.map(y =>
-        YEAR_GROUPED_POSTS[y].map(p => (
-          <li key={p.id}>
-            <Link key={p.id} href={`/${y}/${pageMeta(p).slug}`} onClick={() => onClose()}>
-              {pageMeta(p).title}
-            </Link>
-          </li>
-        ))
-      )}
+      {YEARS.map(y => (
+        <div key={y}>
+          <li className={css.year}>{y}</li>
+          <ul className={css.articleList}>
+            {YEAR_GROUPED_POSTS[y].map(p => (
+              <li key={p.id} className={css.articleListRow}>
+                <Link key={p.id} href={`/${y}/${pageMeta(p).slug}`} onClick={() => onClose()}>
+                  {pageMeta(p).title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </ul>
   )
 }
