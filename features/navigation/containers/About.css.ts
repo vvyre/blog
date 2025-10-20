fbd7509a2174b7450bb096a278c5acfa8f0802a9
vvyre <seung.yoon.yu@gmail.com>
2025-10-20 18:10:45 +0900

    About 노션 문서 렌더 및 스타일 적용

---
+++ features/navigation/containers/About.css.ts
@@ -0,0 +1,11 @@
+import { style } from '@vanilla-extract/css'
+
+export const aboutFrame = style({
+  height: '100%',
+  backgroundColor: 'transparent',
+  width: 'clamp(20rem, 100%, 45rem)',
+  margin: '0 auto',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
