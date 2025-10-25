58df2ccb5ab0770942b5d0c8cfac170354441f60
vvyre <seung.yoon.yu@gmail.com>
2025-10-25 13:25:49 +0900

    로직 변경에 따른 UI 텍스트 변경

--- features/postList/components/PostListHeader.tsx
+++ features/postList/components/PostListHeader.tsx
@@ -4,7 +4,7 @@ export function PostListHeader() {
   return (
     <div className={css.frame}>
       <h1 className={css.title}>
-        <span>RANDOM 5</span>
+        <span>RANDOM 7</span>
       </h1>
       <ShuffleBtn />
     </div>
