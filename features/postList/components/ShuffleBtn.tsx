3df8800ddd01b0ac4578509b3550f36dff1ca5fa
vvyre <seung.yoon.yu@gmail.com>
2025-10-29 15:12:17 +0900

    이벤트 핸들링을 조금 더 선언적으로 관리: 애니메이션 지속 시간을 CSS로 완전 이관

--- features/postList/components/ShuffleBtn.tsx
+++ features/postList/components/ShuffleBtn.tsx
@@ -2,7 +2,7 @@
 import { ReloadIcon } from '@radix-ui/react-icons'
 import clsx from 'clsx'
 import { useSetAtom } from 'jotai'
-import { useState } from 'react'
+import { type AnimationEventHandler, type MouseEventHandler, useState } from 'react'
 import { color } from 'styles/vars/color.css'
 import { shuffleAtom } from '../postList.atom'
 import * as css from './ShuffleBtn.css'
@@ -11,18 +11,18 @@ export function ShuffleBtn() {
   const s = useSetAtom(shuffleAtom)
   const shuffle = () => s(prev => prev + 1)
   const [rotating, setRotating] = useState<boolean>(false)
-  const handleRotating = () => {
-    setRotating(true)
-    setTimeout(() => setRotating(false), 350)
-  }
 
-  const onClick = () => {
-    handleRotating()
+  const handleClick: MouseEventHandler = () => {
+    setRotating(true)
     shuffle()
   }
 
+  const handleAnimationEnd: AnimationEventHandler = () => {
+    setRotating(false)
+  }
+
   return (
-    <button type="button" onClick={onClick} className={clsx(css.shuffleBtn, rotating && css.rotate)}>
+    <button type="button" onClick={handleClick} onAnimationEnd={handleAnimationEnd} className={clsx(css.shuffleBtn, rotating && css.rotate)}>
       <ReloadIcon color={color.notion_gray} width="18" height="18" />
     </button>
   )
