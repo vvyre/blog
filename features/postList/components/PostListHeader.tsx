0783f08a254e1742a72c63eb76f06713df50213f
vvyre <seung.yoon.yu@gmail.com>
2025-10-29 16:25:51 +0900

    cache components 적용, hydration error로 인한 글 리스트 셔플 기능 일시 해제

--- features/postList/components/PostListHeader.tsx
+++ features/postList/components/PostListHeader.tsx
@@ -1,12 +1,10 @@
 import * as css from './PostListHeader.css'
-import { ShuffleBtn } from './ShuffleBtn'
 export function PostListHeader() {
   return (
     <div className={css.frame}>
       <h1 className={css.title}>
-        <span>RANDOM 5</span>
+        <span>(x_x)</span>
       </h1>
-      <ShuffleBtn />
     </div>
   )
 }
--- features/postList/containers/PostListContainer.tsx
+++ features/postList/containers/PostListContainer.tsx
@@ -1,16 +1,11 @@
-'use client'
-import { useAtomValue } from 'jotai'
 import { PostListHeader } from '../components/PostListHeader'
 import { PostListView } from '../components/PostListView'
-import { randomPostsAtom } from '../postList.atom'
 
 export function PostListContainer() {
-  const randomPosts = useAtomValue(randomPostsAtom)
-
   return (
     <>
       <PostListHeader />
-      <PostListView posts={randomPosts} />
+      <PostListView />
     </>
   )
 }
