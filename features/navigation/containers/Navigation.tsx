28994b91a1fbb142b6bdf2a0f0b466e3c07d1ce2
vvyre <seung.yoon.yu@gmail.com>
2025-10-30 12:53:08 +0900

    테마 변경 버튼 추가

--- features/navigation/containers/Navigation.tsx
+++ features/navigation/containers/Navigation.tsx
@@ -1,7 +1,9 @@
 'use client'
-import { HamburgerMenuIcon, InfoCircledIcon } from '@radix-ui/react-icons'
+import { useIsomorphicLayoutEffect } from '@frfla/react-hooks'
+import { HamburgerMenuIcon, InfoCircledIcon, MoonIcon, SunIcon } from '@radix-ui/react-icons'
 import { About } from 'features/navigation/containers/About'
-import { type MouseEvent, useContext } from 'react'
+import { ThemeContext } from 'features/theme'
+import { type MouseEvent, useContext, useState } from 'react'
 import { ExpandedNav } from '../components/ExpandedNav'
 import { HamburgerMenu } from './HamburgerMenu'
 import { MenuBtn } from './MenuButton'
@@ -17,6 +19,7 @@ export function Navigation() {
 }
 
 function NavigationContent() {
+  const { theme, toggleTheme } = useContext(ThemeContext)
   const { key, setOpenState } = useContext(NavContext)
   const handleMenuClose = () => setOpenState({ type: 'close' })
   const handleMenuButton = (e: MouseEvent, to: MenuKeys) => {
@@ -36,12 +39,29 @@ function NavigationContent() {
     }
   }
 
+  //Theme Hydration Error 방지
+  const [mounted, setMounted] = useState(false)
+  useIsomorphicLayoutEffect(() => setMounted(true), [])
+  if (!mounted) return null
+
   return (
     <>
       <div className={css.frame}>
-        <MenuBtn onClick={e => handleMenuButton(e, 'hamburger')}>
-          <HamburgerMenuIcon width="21" height="21" />
-        </MenuBtn>
+        <div className={css.rightButtonGroup}>
+          <MenuBtn onClick={toggleTheme}>
+            {(() => {
+              switch (theme) {
+                case 'dark':
+                  return <SunIcon width="21" height="21" />
+                case 'light':
+                  return <MoonIcon width="21" height="21" />
+              }
+            })()}
+          </MenuBtn>
+          <MenuBtn onClick={e => handleMenuButton(e, 'hamburger')}>
+            <HamburgerMenuIcon width="21" height="21" />
+          </MenuBtn>
+        </div>
         <MenuBtn onClick={e => handleMenuButton(e, 'about')}>
           <InfoCircledIcon width="21" height="21" />
         </MenuBtn>
