426ee7643804e84bf1f2c226ef819be4d8f52119
vvyre <seung.yoon.yu@gmail.com>
2025-10-30 12:50:49 +0900

    라이트/다크 테마 전환 기능 추가

---
+++ features/theme/ThemeProvider.tsx
@@ -0,0 +1,47 @@
+'use client'
+import { useIsomorphicLayoutEffect } from '@frfla/react-hooks'
+import { useMediaQuery } from 'hooks/useMediaQuery.hook'
+import { createContext, type Dispatch, type ReactNode, type SetStateAction, useCallback, useState } from 'react'
+
+export type Theme = 'dark' | 'light'
+export type ThemeContext = {
+  theme: Theme
+  setTheme: Dispatch<SetStateAction<Theme>>
+  toggleTheme: () => void
+}
+
+const initialThemeContextValue: ThemeContext = {
+  theme: 'light',
+  setTheme: () => {},
+  toggleTheme: () => {},
+}
+
+export const ThemeContext = createContext<ThemeContext>(initialThemeContextValue)
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const localStorageKey = `SeyuInTheme`
+  const localSetting = typeof window !== 'undefined' ? (localStorage.getItem(localStorageKey) as Theme | null) : null
+  const prefersDark = useMediaQuery('(prefers-color-scheme: dark)')
+
+  const [theme, setTheme] = useState<Theme>(() => localSetting ?? (prefersDark ? 'dark' : 'light'))
+
+  useIsomorphicLayoutEffect(() => {
+    console.log(theme, localStorage.getItem(localStorageKey))
+    document.body.dataset.theme = theme
+    localStorage.setItem(localStorageKey, theme)
+  }, [theme])
+
+  const toggleTheme = useCallback(() => setTheme(t => (t === 'light' ? 'dark' : 'light')), [])
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        setTheme,
+        toggleTheme,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  )
+}
