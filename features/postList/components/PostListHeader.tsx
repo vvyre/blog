1b6d52b23d195d107b9576becc93e4395c5041eb
vvyre <seung.yoon.yu@gmail.com>
2025-11-01 16:18:31 +0900

    스타일 가이드에 따른 함수 분리

--- features/postList/components/PostListHeader.tsx
+++ features/postList/components/PostListHeader.tsx
@@ -1,16 +1,22 @@
-import dayjs from 'dayjs'
+import dayjs, { type Dayjs } from 'dayjs'
 import { cacheLife } from 'next/cache'
 import * as css from './PostListHeader.css'
 
 export async function PostListHeader() {
-  'use cache' // deterministic render by react.cache()
-  cacheLife('days')
-  const day = dayjs().day()
-  const eyes = ['∗', 'O', '@', '+', '×', '⌃', 'Θ']
-  const index = (day + 6) % 7 // monday -> 0
-  const eye = eyes[index]
-  const date = dayjs().date()
-  const title = `${eye}${'＿'.repeat(date)}${eye}`
+  const dailySiteTitle = (now: Dayjs) => {
+    'use cache' // deterministic render by react.cache()
+    cacheLife('days')
+
+    const date = now.date()
+    const day = now.day()
+    const eyes = ['∗', 'O', '@', '+', '×', '⌃', 'Θ']
+    const index = (day + 6) % 7 // monday -> 0
+    const eye = eyes[index]
+
+    return `${eye}${'＿'.repeat(date)}${eye}`
+  }
+
+  const title = dailySiteTitle(dayjs())
 
   return (
     <div className={css.frame}>
