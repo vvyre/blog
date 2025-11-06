2270a776168fda23c06b2763e2f0a1654f3c2bea
vvyre <seung.yoon.yu@gmail.com>
2025-11-06 16:30:02 +0900

    현재 페이지를 링크하는 경우 새 창으로 열지 않도록 변경

--- features/notion/components/richText/RichText.tsx
+++ features/notion/components/richText/RichText.tsx
@@ -1,12 +1,17 @@
+'use client'
 import type { RichTextItemResponse } from '@notionhq/client/build/src/api-endpoints'
+import { usePathname } from 'next/navigation'
 import { Annotations } from './RTAnnotations'
 import * as css from './RTLink.css'
 
 export function RichText({ richText }: { richText: RichTextItemResponse }) {
-  const isLink = richText.href
-  if (isLink) {
+  const p = usePathname()
+  const href = richText.href
+
+  if (href) {
+    const target = href.includes(p) ? '_self' : '_blank'
     return (
-      <a href={isLink} className={css.link} target="_blank" rel="noreferrer">
+      <a href={href} className={css.link} target={target} rel="noreferrer">
         <Annotations richText={richText}>{richText.plain_text}</Annotations>
       </a>
     )
