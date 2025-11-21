136fded4b29b94e5a6c278859ddb1b7ff3258c07
vvyre <seung.yoon.yu@gmail.com>
2025-11-21 14:50:30 +0900

    타입 안정성 확보, 외부에서 스타일을 확장할 수 있도록 조치

--- features/navigation/containers/MenuButton.tsx
+++ features/navigation/containers/MenuButton.tsx
@@ -1,27 +1,28 @@
 'use client'
-import Link from 'next/link'
-import { type ComponentProps, type ElementType, ReactNode, useState } from 'react'
-import * as css from './Navigation.css'
 import { useBooleanState } from '@frfla/react-hooks'
+import clsx from 'clsx'
+import Link from 'next/link'
+import type { ComponentProps, ElementType, MouseEvent, ReactNode } from 'react'
+import * as css from './Navigation.css'
 
 type MenuBtnProps<T extends ElementType> = {
   as?: T
   onHoverComponent?: (hover: boolean) => ReactNode
-} & Omit<ComponentProps<T>, 'as'> //Link 자체가 as props을 지원하기 때문에 제거
+} & Omit<ComponentProps<T>, 'as'> //Link 자체가 as props을 지원하기 때문에 제
 
 type ExtendedElementType = ElementType | typeof Link
 
 export function MenuBtn<T extends ExtendedElementType = 'button'>({ as, onHoverComponent, className: _className, ...props }: MenuBtnProps<T>) {
   const [hovered, hover, exit] = useBooleanState(false)
   const Comp = as ?? 'button'
-  const className = css.categoryBtn
+  const className = clsx(css.categoryBtn, _className)
 
-  const handleMouseEnter = (e: any) => {
+  const handleMouseEnter = (e: MouseEvent) => {
     hover()
     props.onMouseEnter?.(e)
   }
 
-  const handleMouseLeave = (e: any) => {
+  const handleMouseLeave = (e: MouseEvent) => {
     exit()
     props.onMouseLeave?.(e)
   }
