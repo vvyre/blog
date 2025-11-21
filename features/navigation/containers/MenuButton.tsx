99427861ff5e3f5670c598c64a0518f1192de9db
vvyre <seung.yoon.yu@gmail.com>
2025-11-21 13:55:35 +0900

    테마 변경 버튼에 툴팁 인터랙션 추가

--- features/navigation/containers/MenuButton.tsx
+++ features/navigation/containers/MenuButton.tsx
@@ -1,20 +1,49 @@
 'use client'
 import Link from 'next/link'
-import type { ComponentProps, ElementType } from 'react'
+import { type ComponentProps, type ElementType, ReactNode, useState } from 'react'
 import * as css from './Navigation.css'
+import { useBooleanState } from '@frfla/react-hooks'
 
 type MenuBtnProps<T extends ElementType> = {
   as?: T
+  onHoverComponent?: (hover: boolean) => ReactNode
 } & Omit<ComponentProps<T>, 'as'> //Link 자체가 as props을 지원하기 때문에 제거
 
 type ExtendedElementType = ElementType | typeof Link
 
-export function MenuBtn<T extends ExtendedElementType = 'button'>({ as, ...props }: MenuBtnProps<T>) {
+export function MenuBtn<T extends ExtendedElementType = 'button'>({ as, onHoverComponent, className: _className, ...props }: MenuBtnProps<T>) {
+  const [hovered, hover, exit] = useBooleanState(false)
   const Comp = as ?? 'button'
   const className = css.categoryBtn
 
-  if (Comp === Link) return <Link {...(props as unknown as ComponentProps<typeof Link>)} className={className} data-menu-trigger />
+  const handleMouseEnter = (e: any) => {
+    hover()
+    props.onMouseEnter?.(e)
+  }
 
-  const C = Comp as ElementType //Link를 제외하고 재타입
-  return <C {...(props as ComponentProps<typeof C>)} className={className} data-menu-trigger />
+  const handleMouseLeave = (e: any) => {
+    exit()
+    props.onMouseLeave?.(e)
+  }
+
+  return (
+    <>
+      {(() => {
+        if (Comp === Link) {
+          return (
+            <Link
+              href={props.href}
+              {...props}
+              className={className}
+              data-menu-trigger
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            />
+          )
+        }
+        return <Comp {...props} className={className} data-menu-trigger onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} />
+      })()}
+      {onHoverComponent?.(hovered)}
+    </>
+  )
 }
