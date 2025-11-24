d3b4d56ace2fcc6b285f9b559ad1500a0ab59e0b
vvyre <seung.yoon.yu@gmail.com>
2025-11-24 18:47:34 +0900

    파일 네이밍 컨벤션에 맞게 리네임

--- features/notion/utils/getHeadingId.ts
+++ features/notion/utils/getHeadingId.ts
@@ -1,7 +1,7 @@
 import type { RichTextItemResponse } from '@notionhq/client'
 
 export function getHeadingId(richText: RichTextItemResponse[]) {
-  const text = richText[0].plain_text
+  const text = richText.map(t => t.plain_text).join(' ')
   //특수문자 제거
   let result = text
   result = result.replace(/[^\w가-힣\s]/g, '')
