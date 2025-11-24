46b5106d5b52e83295be04e06bd984ce0298824c
vvyre <seung.yoon.yu@gmail.com>
2025-11-24 18:48:18 +0900

    중복 코드 삭제 및 리팩토링

--- features/tableOfContents/components/HeadingItem.tsx
+++ features/tableOfContents/components/HeadingItem.tsx
@@ -1,36 +1,31 @@
-import { getHeadingId } from 'features/notion/utils/get-heading-id'
+import { getHeadingId } from 'features/notion/utils/getHeadingId'
 import { getPlainText } from 'features/notion/utils/getPlainText.util'
 import { getHeadingLevel } from '../util/getHeadingLevel'
 import type { TableOfContentBlock } from '../util/processHeadings'
 import * as css from './TableOfContents.css'
 
 export function HeadingItem({ heading }: { heading: TableOfContentBlock }) {
-  const text = (() => {
+  const richText = (() => {
     switch (heading.type) {
       case 'heading_1':
-        return getPlainText(heading.heading_1.rich_text)
+        return heading.heading_1.rich_text
       case 'heading_2':
-        return getPlainText(heading.heading_2.rich_text)
+        return heading.heading_2.rich_text
       case 'heading_3':
-        return getPlainText(heading.heading_3.rich_text)
+        return heading.heading_3.rich_text
+      default:
+        return []
     }
   })()
-  const url = (() => {
-    switch (heading.type) {
-      case 'heading_1':
-        return getHeadingId(heading.heading_1.rich_text)
-      case 'heading_2':
-        return getHeadingId(heading.heading_2.rich_text)
-      case 'heading_3':
-        return getHeadingId(heading.heading_3.rich_text)
-    }
-  })()
+
+  const text = getPlainText(richText)
+  const href = `#${getHeadingId(richText)}`
 
   const className = css.listitem[getHeadingLevel(heading)]
 
   return (
     <li className={className}>
-      <a className={css.listLink} href={`#${url}`} target="_self">
+      <a className={css.listLink} href={href} target="_self">
         <span>{text}</span>
       </a>
       {heading.childrenHeadings.length > 0 && (
