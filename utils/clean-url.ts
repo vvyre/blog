ef85b48910b0f01f7c1e288913942cd90670bd36
vvyre <seung.yoon.yu@gmail.com>
2025-11-24 15:00:23 +0900

    cleanUrl을 사용할 때 파라미터를 혼동하지 않도록 리팩토링

--- utils/clean-url.ts
+++ utils/clean-url.ts
@@ -1,6 +1,9 @@
-export function cleanUrl(url: string) {
+import { RichTextItemResponse } from '@notionhq/client'
+
+export function cleanUrl(richText: RichTextItemResponse[]) {
+  const text = richText[0].plain_text
   //특수문자 제거
-  let result = url
+  let result = text
   result = result.replace(/[^\w가-힣\s]/g, '')
   //공백 대체
   result = result.replace(/\s/g, '-')
