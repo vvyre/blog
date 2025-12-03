683e00676356992a6ed320816d7d1473778202c7
vvyre <seung.yoon.yu@gmail.com>
2025-12-03 21:45:16 +0900

    Feat: katex로 notion equation 블록 렌더 (#31)

--- features/tableOfContents/containers/TableOfContentsContainer.tsx
+++ features/tableOfContents/containers/TableOfContentsContainer.tsx
@@ -8,11 +8,13 @@ export function TableOfContentsContainer() {
   const headings = useAtomValue(tableOfContentsAtom)
 
   return (
-    <div className={css.frame}>
-      <h2 className={css.title}>목차</h2>
-      <div className={css.tocFrame}>
-        <RenderHeadings headings={headings} />
+    headings.length > 0 && (
+      <div className={css.frame}>
+        <h2 className={css.title}>목차</h2>
+        <div className={css.tocFrame}>
+          <RenderHeadings headings={headings} />
+        </div>
       </div>
-    </div>
+    )
   )
 }
