7dd5b118232c696b73b6d084ea026af7e64f146c
vvyre <seung.yoon.yu@gmail.com>
2025-12-13 18:55:22 +0900

    블록 처리 및 변형을 간단하게 추가할 수 있도록 리팩토링

---
+++ features/notion/utils/processBlock/processor/image.ts
@@ -0,0 +1,13 @@
+import type { ImageBlockObjectResponse } from '@notionhq/client/build/src/api-endpoints'
+import { registerProcessor } from '../registry'
+import type { ExtendedImageBlockObjectResponse } from 'features/notion/types'
+
+export const getBlurredImg = async (block: ImageBlockObjectResponse): Promise<ExtendedImageBlockObjectResponse> => {
+  //vercel fn timeout으로 인해 임시 기능 삭제
+  return {
+    ...block,
+    blurDataURL: '',
+  }
+}
+
+registerProcessor('image', async block => await getBlurredImg(block))
